991bc5003b0b777c657e5ff1c72632ccda7e047f
popovmaximmichailovich-oss <popov.maxim.michailovich@gmail.com>
2026-05-18 10:34:54 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'medorg-checklist-v18';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html')))); });
